bcab56e57ce78d731cb2c73c5965139f8ca0b663
maichomper <maichomper@gmail.com>
2015-08-21 13:51:07 -0500

    finished seting up configuration and added versioning through headers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
-Rails.application.routes.draw do
- 
+require 'api_constraints'
+
+Sil::Application.routes.draw do
+  # API definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1,
+                  constraints: ApiConstraints.new( version: 1, default: true ) do
+      # List of resources 
+    end
+  end
 end
