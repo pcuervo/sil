4b8f90d864f223c98a118774cbdb2fc8f991598c
maichomper <maichomper@gmail.com>
2015-08-21 14:41:01 -0500

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Sil::Application.routes.draw do
     scope module: :v1,
                   constraints: ApiConstraints.new( version: 1, default: true ) do
       # List of resources 
-      resources :users, :only => [:show]
+      resources :users, :only => [:show, :create]
     end
   end
 end
