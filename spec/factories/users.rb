ba1e01a368865d18166bd0a304528c9fe21a1a6e
maichomper <maichomper@gmail.com>
2015-08-21 14:20:21 -0500

    Adds show action to the users controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,10 @@
 FactoryGirl.define do
   factory :user do
+  	#first_name "Juan"
+  	#last_name "Pérez"
     email { FFaker::Internet.email }
     password "holama123"
     password_confirmation "holama123"
+    #role "admin"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,20 @@ describe User do
 
   subject { @user }
 
+  #it { should respond_to(:first_name) }
+  #it { should respond_to(:last_name) }
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  #it { should respond_to(:role) }
+
+  #it { should validate_presence_of(:first_name) }
+  #it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+	it { should validate_uniqueness_of(:email) }
+	it { should validate_confirmation_of(:password) }
+	#it { should validate_presence_of(:role) }
+	it { should allow_value('example@domain.com').for(:email) }
 
   it { should be_valid }
 end
