b57a28166c7f3b712f1e86e392b766e785310d85
maichomper <maichomper@gmail.com>
2015-08-21 14:56:21 -0500

    Adds update action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,6 +15,17 @@ class Api::V1::UsersController < ApplicationController
 		render json: { errors: user.errors }, status: 422
 	end
 
+	def update
+		user = User.find(params[:id])
+
+		if user.update(user_params)
+	    render json: user, status: 200, location: [:api, user]
+	    return
+	  end
+
+	 	render json: { errors: user.errors }, status: 422
+	end
+
 	private	
 
 		def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Sil::Application.routes.draw do
     scope module: :v1,
                   constraints: ApiConstraints.new( version: 1, default: true ) do
       # List of resources 
-      resources :users, :only => [:show, :create]
+      resources :users, :only => [:show, :create, :update]
     end
   end
 end
