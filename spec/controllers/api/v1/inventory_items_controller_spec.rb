a7fb8d1c3777b2c2d15ffff24143b3890126e964
maichomper <maichomper@gmail.com>
2015-08-27 19:21:06 -0500

    Adds public inventory_item endpoints(show, index)

--- spec/controllers/api/v1/inventory_items_controller_spec.rb
+++ spec/controllers/api/v1/inventory_items_controller_spec.rb
@@ -28,4 +28,46 @@ describe Api::V1::InventoryItemsController do
 
     it { should respond_with 200 }
   end
+
+  describe "POST #create" do
+    context "when is succesfully created" do
+      before(:each) do
+        user = FactoryGirl.create :user
+        @inventory_item_attributes = FactoryGirl.attributes_for :inventory_items
+        api_authorization_header user.auth_token
+        post :create, { user_id: user.id, inventory_item: @inventory_item_attributes }
+      end
+
+      it "renders the json representation for the inventory item just created" do
+        inventory_item_response = json_response
+        expect(inventory_item_response[:name]).to eql @inventory_item_attributes[:name]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        user = FactoryGirl.create :user
+        @invalid_inventory_item_attributes = { :serial_number = "hi mom" }
+
+        api_authorization_header user.auth_token
+        post :create, { user_id: user.id, inventory_item: @invalid_inventory_item_attributes }
+      end
+
+      it "renders an errors json" do
+        inventory_item_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the inventory item could not be created" do
+        inventory_item_response = json_response
+        expect(inventory_item_response[:errors][:name]).to include "must have name"
+      end
+
+      it { should respond_with 422 }
+
+    end
+  end
+
 end
