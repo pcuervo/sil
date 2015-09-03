4b723bc6c98c8de3e5f964fbfff77f952b3b2a87
maichomper <maichomper@gmail.com>
2015-09-03 15:08:40 -0500

    adds create and update action to users routes

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Sil::Application.routes.draw do
 
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1 , default: true) do
-      resources :users, :only => [:index, :show]
+      resources :users, :only => [:index, :show, :create, :update]
     end
   end
 
