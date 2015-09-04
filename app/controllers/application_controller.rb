d2b51b9e371a7d8321808e8419e64d3595ce5e8c
maichomper <maichomper@gmail.com>
2015-09-03 19:53:48 -0500

    add allow_ajax_request_from_other_domains to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,14 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_filter :allow_ajax_request_from_other_domains
+
+  def allow_ajax_request_from_other_domains
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = '*'
+  end
+
   protected
 
   def configure_permitted_parameters
